693a84c613be5b8b3ee697df4ebddb982d2c7861
Moritz Roessler <morrossl@publicisgroupe.net>
2023-11-18 16:28:08 +0100

    feat: add social icons

--- src/App.tsx
+++ src/App.tsx
@@ -35,24 +35,7 @@ import { ProjectCard } from './components/Projects';
 import { differenceInBusinessDays, format } from 'date-fns';
 import { locales } from './components/WorkExperienceItem';
 import Markdown from './components/Markdown';
-
-const SOIcon = () => {
-  return (
-    <svg
-      aria-hidden="true"
-      className="svg-icon iconLogoGlyphMd native native"
-      width="24"
-      height="24"
-      viewBox="0 0 32 37"
-    >
-      <path d="M26 33v-9h4v13H0V24h4v9h22Z" fill="#BCBBBB"></path>
-      <path
-        d="m21.5 0-2.7 2 9.9 13.3 2.7-2L21.5 0ZM26 18.4 13.3 7.8l2.1-2.5 12.7 10.6-2.1 2.5ZM9.1 15.2l15 7 1.4-3-15-7-1.4 3Zm14 10.79.68-2.95-16.1-3.35L7 23l16.1 2.99ZM23 30H7v-3h16v3Z"
-        fill="#F48024"
-      ></path>
-    </svg>
-  );
-};
+import { StackOverflowIcon } from './lib/icons';
 
 function App() {
   const { toPDF, targetRef } = usePDF({ filename: `CV - ${data.name}.pdf` });
@@ -189,7 +172,7 @@ function App() {
                       )}
                       {learning.so && (
                         <ListItemIcon sx={{ minWidth: '40px' }}>
-                          <SOIcon />
+                          <StackOverflowIcon />
                         </ListItemIcon>
                       )}
                       <ListItemText
@@ -242,6 +225,9 @@ export const SocialCard = () => {
           return (
             <Grid item xs={12} md={12}>
               <ListItem dense>
+                <ListItemIcon sx={{ minWidth: '40px' }}>
+                  {social.Icon && <social.Icon />}
+                </ListItemIcon>
                 <ListItemText
                   primary={<Link href={social.url}>{social.url}</Link>}
                   secondary={
--- src/data/social.ts
+++ src/data/social.ts
@@ -1,16 +1,23 @@
+import { StackOverflowIcon } from '@/lib/icons';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
 export const social = [
   {
     title: 'Github',
+    Icon: GitHubIcon,
     secondary: 'C5H8NNaO4',
     url: 'https://github.com/C5H8NNaO4',
   },
   {
     title: 'LinkedIn',
+    Icon: LinkedInIcon,
     secondary: 'Moritz Roessler',
     url: 'https://www.linkedin.com/in/moritz-roessler-666b18175/',
   },
   {
     title: 'Stackoverflow',
+    Icon: StackOverflowIcon,
     secondary: 'Top 5% overall',
     url: 'https://stackoverflow.com/users/1487756/moritz-roessler',
   },
