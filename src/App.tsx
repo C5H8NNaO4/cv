310663e33bb9c12a20c0217cdf61dc906df33011
Moritz Roessler <morrossl@publicisgroupe.net>
2024-09-02 12:11:21 +0200

    fix: improve caching

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import './App.css';
 import './lib/i18n';
 import * as Harvard from '@/components/templates/harvard/harvard';
 
-
 function App() {
   // const printing = useMediaQuery('print');
   const { Layout } = Harvard;
 
   return (
-    <div id="root">
+    <div>
       <Layout />
       {/* <Box sx={{ mt: !printing ? '100vh' : 0 }} />
       <Page>
